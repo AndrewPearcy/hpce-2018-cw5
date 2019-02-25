761de0da0327e61c235a18d90088f3a55fbea576
Db2515 <daniel@boulby.plus.com>
2019-02-25 17:32:49 +0000

    Add tbb parallel implementation of rank

--- provider/puzzles.cpp
+++ provider/puzzles.cpp
@@ -9,6 +9,7 @@
 // TODO: include your engine headers
 #include "decompose_tbb.hpp"
 #include "ising_tbb.hpp"
+#include "rank_tbb.hpp"
 
 void puzzler::PuzzleRegistrar::UserRegisterPuzzles() {
   Register("decompose.ref", std::make_shared<puzzler::DecomposePuzzle>());
@@ -19,6 +20,7 @@ void puzzler::PuzzleRegistrar::UserRegisterPuzzles() {
   // TODO: Register more engines!
   Register("ising.tbb", std::make_shared<puzzler::IsingTbbProvider>());
   Register("decompose.tbb", std::make_shared<puzzler::DecomposeTbbProvider>());
+  Register("rank.tbb", std::make_shared<puzzler::RankTbbProvider>());
 
   // Note that you can register the same engine twice under different names, for
   // example you could register the same engine for "ising.tbb" and "ising.opt"
---
+++ provider/rank_tbb.hpp
@@ -0,0 +1,79 @@
+#ifndef user_rank_tbb_hpp
+#define user_rank_tbb_hpp
+
+#include "puzzler/puzzles/rank.hpp"
+
+#include "tbb/parallel_for.h"
+
+namespace puzzler{
+class RankTbbProvider
+  : public puzzler::RankPuzzle
+{
+public:
+  RankTbbProvider()
+  {}
+
+	virtual void Execute(
+			   puzzler::ILog *log,
+			   const puzzler::RankInput *pInput,
+				puzzler::RankOutput *pOutput
+			   ) const override
+	{
+      const std::vector<std::vector<uint32_t> > &edges=pInput->edges;
+      float tol=pInput->tol;
+      unsigned n=edges.size();
+
+      log->LogInfo("Starting iterations.");
+      std::vector<float> curr(n, 0.0f);
+      curr[0]=1.0;
+      std::vector<float> next(n, 0.0f);
+      float dist = norm(curr, next);
+      while(tol < dist) {
+        log->LogVerbose("dist=%g", dist);
+        iteration(log, n, edges, &curr[0], &next[0]);
+        std::swap(curr, next);
+        dist=norm(curr, next);
+      }
+      
+      pOutput->ranks= (std::vector<float>) curr;
+      
+      log->LogInfo("Finished");
+	}
+
+
+  void iteration(ILog *log, unsigned n, 
+                  const std::vector<std::vector<uint32_t> > &edges, 
+                  const float *current, 
+                  float *next) const
+    {
+      tbb::parallel_for((unsigned) 0, n, [&](unsigned i){
+        next[i]=0;
+      });
+      std::vector<tbb::atomic<float>> atomic_next(n, 0.0f);
+      for(unsigned i=0; i<n; i++){
+        for(unsigned j=0; j<edges[i].size(); j++){
+          int dst=edges[i][j];
+          atomic_next[dst] = atomic_next[dst] + (current[i] / edges[i].size());
+        }
+      }
+
+      tbb::parallel_for((unsigned) 0, n, [&](unsigned i) {
+        next[i] = atomic_next[i].load(); 
+      });
+
+      tbb::atomic<double> total=0;
+      tbb::parallel_for((unsigned) 0, n, [&](unsigned i){
+        next[i] = (current[i] * 0.3  + next[i] * 0.7 );
+        total = total + next[i];
+      });
+      log->LogVerbose("  total=%g", total);
+      tbb::parallel_for((unsigned) 0, n ,[&](unsigned i){
+        next[i] = next[i] / total;
+        log->LogVerbose("    c[%u] = %g", i, next[i]);
+      });
+    }
+
+};
+};
+
+#endif
