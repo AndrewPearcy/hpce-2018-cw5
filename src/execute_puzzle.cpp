bd841ccad3abd551d09fabe99d596927128db904
Db2515 <daniel@boulby.plus.com>
2019-02-27 18:56:43 +0000

    Add test and scoring script

--- src/execute_puzzle.cpp
+++ src/execute_puzzle.cpp
@@ -1,76 +1,83 @@
 
 #include "puzzler/puzzler.hpp"
 
+#include <chrono>
 #include <iostream>
 
+int main(int argc, char *argv[]) {
+  puzzler::PuzzleRegistrar::UserRegisterPuzzles();
 
-int main(int argc, char *argv[])
-{
-   puzzler::PuzzleRegistrar::UserRegisterPuzzles();
+  if (argc < 2) {
+    fprintf(stderr, "execute_puzzle [engine|'ref'] [logLevel]\n");
+    exit(1);
+  }
 
-   if(argc<2){
-      fprintf(stderr, "execute_puzzle [engine|'ref'] [logLevel]\n");
-      exit(1);
-   }
+  try {
+    int isReference = !strcmp(argv[1], "ref");
+    int logLevel = 3;
 
-   try{
-      int isReference=!strcmp(argv[1], "ref");
-      int logLevel=3;
-      
-      if(argc>2){
-          logLevel=atoi(argv[2]);
-      }
+    if (argc > 2) {
+      logLevel = atoi(argv[2]);
+    }
 
-      std::shared_ptr<puzzler::ILog> logDest=std::make_shared<puzzler::LogDest>("execute_puzzle", logLevel);
-      logDest->Log(puzzler::Log_Info, "Created log.");
+    std::shared_ptr<puzzler::ILog> logDest =
+        std::make_shared<puzzler::LogDest>("execute_puzzle", logLevel);
+    logDest->Log(puzzler::Log_Info, "Created log.");
 
-      std::shared_ptr<puzzler::Puzzle::Input> input;
-      {
-         puzzler::StdinStream src;
-         puzzler::PersistContext ctxt(&src, false);
+    std::shared_ptr<puzzler::Puzzle::Input> input;
+    {
+      puzzler::StdinStream src;
+      puzzler::PersistContext ctxt(&src, false);
 
-         input=puzzler::PuzzleRegistrar().LoadInput(ctxt);
-      }
+      input = puzzler::PuzzleRegistrar().LoadInput(ctxt);
+    }
 
-      logDest->Log(puzzler::Log_Info, "Loaded input, puzzle=%s", input->PuzzleName().c_str());
+    logDest->Log(puzzler::Log_Info, "Loaded input, puzzle=%s",
+                 input->PuzzleName().c_str());
 
-      std::shared_ptr<puzzler::Puzzle> puzzle;
-      if(isReference){
-         puzzle=puzzler::PuzzleRegistrar().LookupPuzzle(input->PuzzleName());
-      }else{
-         puzzle=puzzler::PuzzleRegistrar().LookupEngine(argv[1]);
-      }
+    std::shared_ptr<puzzler::Puzzle> puzzle;
+    if (isReference) {
+      puzzle = puzzler::PuzzleRegistrar().LookupPuzzle(input->PuzzleName());
+    } else {
+      puzzle = puzzler::PuzzleRegistrar().LookupEngine(argv[1]);
+    }
 
-      auto output=puzzle->MakeEmptyOutput(input.get());
+    auto output = puzzle->MakeEmptyOutput(input.get());
 
-      if(isReference){
-         logDest->LogInfo("Begin reference");
-         puzzle->ReferenceExecute(logDest.get(), input.get(), output.get());
-         logDest->LogInfo("Finished reference");
-      }else{
-         logDest->LogInfo("Begin execution");
-         puzzle->Execute(logDest.get(), input.get(), output.get());
-         logDest->LogInfo("Finished execution");
-      }
+    auto start = std::chrono::high_resolution_clock::now();
+    if (isReference) {
+      logDest->LogInfo("Begin reference");
+      puzzle->ReferenceExecute(logDest.get(), input.get(), output.get());
+      logDest->LogInfo("Finished reference");
+    } else {
+      logDest->LogInfo("Begin execution");
+      puzzle->Execute(logDest.get(), input.get(), output.get());
+      logDest->LogInfo("Finished execution");
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout
+        << "TIME: "
+        << srd::chrono::duration<double, std::milli>(end - start).count() / 1000
+        << std::endl;
 
-      {
-         puzzler::StdoutStream dst;
-         puzzler::PersistContext ctxt(&dst, true);
+    {
+      puzzler::StdoutStream dst;
+      puzzler::PersistContext ctxt(&dst, true);
 
-         output->Persist(ctxt);
-      }
+      output->Persist(ctxt);
+    }
 
-   }catch(std::string &msg){
-      std::cerr<<"Caught error string : "<<msg<<std::endl;
-      return 1;
-   }catch(std::exception &e){
-      std::cerr<<"Caught exception : "<<e.what()<<std::endl;
-      return 1;
-   }catch(...){
-      std::cerr<<"Caught unknown exception."<<std::endl;
-      return 1;
-   }
+  } catch (std::string &msg) {
+    std::cerr << "Caught error string : " << msg << std::endl;
+    return 1;
+  } catch (std::exception &e) {
+    std::cerr << "Caught exception : " << e.what() << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Caught unknown exception." << std::endl;
+    return 1;
+  }
 
-   return 0;
+  return 0;
 }
 
