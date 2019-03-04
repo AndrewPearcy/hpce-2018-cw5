37623efc2a539706d749965ac2ff54d0983622f0
db2515 <db2515@ic.ac.uk>
2019-03-04 11:10:44 +0000

    Move opencl code into constructor

--- provider/decompose_opencl.hpp
+++ provider/decompose_opencl.hpp
@@ -36,8 +36,72 @@ std::string LoadSource(const char *fileName) const
 	);
 } 
 
+  cl::Program program;
+  cl::Context context;
+  mutable cl::CommandQueue queue;
+
   DecomposeOpenCLProvider()
-  {}
+  {
+    // OpenCl Code
+	  std::vector<cl::Platform> platforms;
+	
+	  cl::Platform::get(&platforms);
+	  if(platforms.size()==0)
+		  throw std::runtime_error("No OpenCL platforms found.");
+	
+	  std::cerr<<"Found "<<platforms.size()<<" platforms\n";
+	  for(unsigned i=0;i<platforms.size();i++){
+		  std::string vendor=platforms[i].getInfo<CL_PLATFORM_VENDOR>();
+		  std::cerr<<"  Platform "<<i<<" : "<<vendor<<"\n";
+	  }
+	
+	  int selectedPlatform=0;
+	  if(getenv("HPCE_SELECT_PLATFORM")){
+		  selectedPlatform=atoi(getenv("HPCE_SELECT_PLATFORM"));
+	  }
+	  std::cerr<<"Choosing platform "<<selectedPlatform<<"\n";
+	  cl::Platform platform=platforms.at(selectedPlatform);    	
+
+	  std::vector<cl::Device> devices;
+	  platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);	
+	  if(devices.size()==0){
+		  throw std::runtime_error("No opencl devices found.\n");
+	  }
+	
+	  std::cerr<<"Found "<<devices.size()<<" devices\n";
+	  for(unsigned i=0;i<devices.size();i++){
+		  std::string name=devices[i].getInfo<CL_DEVICE_NAME>();
+		  std::cerr<<"  Device "<<i<<" : "<<name<<"\n";
+	  }		
+
+	  int selectedDevice=0;
+	  if(getenv("HPCE_SELECT_DEVICE")){
+		  selectedDevice=atoi(getenv("HPCE_SELECT_DEVICE"));
+	  }
+	  std::cerr<<"Choosing device "<<selectedDevice<<"\n";
+	  cl::Device device=devices.at(selectedDevice);
+	
+	  context = cl::Context(devices);
+	
+	  std::string kernelSource=LoadSource("decompose_kernel.cl");
+
+	  cl::Program::Sources sources;	// A vector of (data,length) pairs
+	  sources.push_back(std::make_pair(kernelSource.c_str(), kernelSource.size()+1));	// push on our single string
+
+	  program = cl::Program(context, sources);
+	  try{
+		  program.build(devices);
+	  }catch(...){
+		  for(unsigned i=0;i<devices.size();i++){
+			  std::cerr<<"Log for device "<<devices[i].getInfo<CL_DEVICE_NAME>()<<":\n\n";
+			  std::cerr<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[i])<<"\n\n";
+		  }	
+		  throw;
+	  }
+
+    queue = cl::CommandQueue(context, device); 
+
+  }
 
 	virtual void Execute(
 			  puzzler::ILog *log,
@@ -50,62 +114,7 @@ std::string LoadSource(const char *fileName) const
     unsigned cc=n;
     uint32_t p=7;
  
- 	// OpenCl Code
-	std::vector<cl::Platform> platforms;
-	
-	cl::Platform::get(&platforms);
-	if(platforms.size()==0)
-		throw std::runtime_error("No OpenCL platforms found.");
-	
-	std::cerr<<"Found "<<platforms.size()<<" platforms\n";
-	for(unsigned i=0;i<platforms.size();i++){
-		std::string vendor=platforms[i].getInfo<CL_PLATFORM_VENDOR>();
-		std::cerr<<"  Platform "<<i<<" : "<<vendor<<"\n";
-	}
-	
-	int selectedPlatform=0;
-	if(getenv("HPCE_SELECT_PLATFORM")){
-		selectedPlatform=atoi(getenv("HPCE_SELECT_PLATFORM"));
-	}
-	std::cerr<<"Choosing platform "<<selectedPlatform<<"\n";
-	cl::Platform platform=platforms.at(selectedPlatform);    	
-
-	std::vector<cl::Device> devices;
-	platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);	
-	if(devices.size()==0){
-		throw std::runtime_error("No opencl devices found.\n");
-	}
-	
-	std::cerr<<"Found "<<devices.size()<<" devices\n";
-	for(unsigned i=0;i<devices.size();i++){
-		std::string name=devices[i].getInfo<CL_DEVICE_NAME>();
-		std::cerr<<"  Device "<<i<<" : "<<name<<"\n";
-	}		
-
-	int selectedDevice=0;
-	if(getenv("HPCE_SELECT_DEVICE")){
-		selectedDevice=atoi(getenv("HPCE_SELECT_DEVICE"));
-	}
-	std::cerr<<"Choosing device "<<selectedDevice<<"\n";
-	cl::Device device=devices.at(selectedDevice);
-	
-	cl::Context context(devices);
-	
-	std::string kernelSource=LoadSource("decompose_kernel.cl");
-
-	cl::Program::Sources sources;	// A vector of (data,length) pairs
-	sources.push_back(std::make_pair(kernelSource.c_str(), kernelSource.size()+1));	// push on our single string
-
-	cl::Program program(context, sources);
-	try{
-		program.build(devices);
-	}catch(...){
-		for(unsigned i=0;i<devices.size();i++){
-			std::cerr<<"Log for device "<<devices[i].getInfo<CL_DEVICE_NAME>()<<":\n\n";
-			std::cerr<<program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[i])<<"\n\n";
-		}	
-		throw;
-	} 
+ 	 
 
 	
     log->LogInfo("Building random matrix");
@@ -118,8 +127,7 @@ std::string LoadSource(const char *fileName) const
     kernel.setArg(0, buffMatrix);
     kernel.setArg(1, seed);
     kernel.setArg(2, p);
-    cl::CommandQueue queue(context, device); 
-    queue.enqueueWriteBuffer(buffMatrix, CL_TRUE, 0, buffSize, &matrix[0]);
+        queue.enqueueWriteBuffer(buffMatrix, CL_TRUE, 0, buffSize, &matrix[0]);
     cl::NDRange offset(0);
     cl::NDRange globalSize(matrix.size());
     cl::NDRange localSize=cl::NullRange;
